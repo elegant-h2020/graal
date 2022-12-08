362d8d02f4769f4b26d13578d27f1793ddf95f5a
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2022-12-08 14:07:42 +0000

    [wip] Add Tornado as library in compiler suite and hotspot

--- compiler/mx.compiler/suite.py
+++ compiler/mx.compiler/suite.py
@@ -172,6 +172,14 @@ suite = {
         },
       },
     },
+    # ------------- TornadoVM -------------
+    "uk.ac.manchester.tornado.drivers.common" : {
+      "path" : "/home/thanos/repositories/tornadoVM2/bin/sdk/share/java/tornado/tornado-drivers-common-0.13.jar",
+      "sourcePath" : "/home/thanos/repositories/tornadoVM2/bin/sdk/share/java/tornado/tornado-drivers-common-0.13.jar",
+      "sha1" : "6e63dd9beea4750e0b8d2158f689d1a946f667df",
+      "optional" : False,
+    },
+
   },
 
   "projects" : {
@@ -558,6 +566,7 @@ suite = {
       "sourceDirs" : ["src"],
       "dependencies" : [
         "JVMCI_HOTSPOT",
+        "uk.ac.manchester.tornado.drivers.common",
         "org.graalvm.compiler.api.runtime",
         "org.graalvm.compiler.replacements",
         "org.graalvm.compiler.printer",
